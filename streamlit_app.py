a73aab7b936ad224235cee2a567a5905234bca8f
NNN-TSUKIYAMA <162134115+NNN-TSUKIYAMA@users.noreply.github.com>
2024-03-05 01:15:28 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from scipy import interpolate
-import matplotlib.pyplot as plt
-from scipy import signal
 
 st.title("心拍変動解析") # タイトル
 st.header("FFTr") # ヘッダー
@@ -13,4 +10,11 @@ if uploaded_file is not None:
     df = pd.read_excel(uploaded_file)
     timestamps = df["Time[s]"]  # 心拍のタイムスタンプデータ
     rri = df["RRIa"] # 心拍間隔データ
+
+# NaN値を除外してデータを抽出
+valid_indices = ~np.isnan(rri)
+valid_timestamps = timestamps[valid_indices]
+valid_rri = rri[valid_indices]
+
+
 
