dcf4be395b776ecbefe7411485e2f1a08485efc7
NNN-TSUKIYAMA <162134115+NNN-TSUKIYAMA@users.noreply.github.com>
2024-03-04 23:41:39 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,24 +1,24 @@
-import streamlit as st
+from io import BytesIO
 import pandas as pd
+import streamlit as st
 
-# Streamlitアプリのタイトルを設定
-st.title("Excelデータの読み込み")
+def df_to_xlsx(df):
+    byte_xlsx = BytesIO()
+    writer_xlsx = pd.ExcelWriter(byte_xlsx, engine="xlsxwriter")
+    df.to_excel(writer_xlsx, index=False, sheet_name="Sheet1")
+    ##-----必要に応じてexcelのフォーマット等を設定-----##
+    workbook = writer_xlsx.book
+    worksheet = writer_xlsx.sheets["Sheet1"]
+    format1 = workbook.add_format({"num_format": "0.00"})
+    worksheet.set_column("A:A", None, format1)
+    writer_xlsx.save()
+    ##---------------------------------------------##
+    workbook = writer_xlsx.book
+    out_xlsx = byte_xlsx.getvalue()
+    return out_xlsx
 
-# ファイルアップロード
-uploaded_file = st.file_uploader("Excelファイルをアップロードしてください", type=['xlsx', 'xls'])
 
-# アップロードされたファイルがある場合
-if uploaded_file is not None:
-    try:
-        # Excelファイルを読み込む
-        df = pd.read_excel(uploaded_file)
-        
-        # 読み込んだデータを表示
-        st.write("読み込んだデータ:")
-        st.write(df)
-        
-    except Exception as e:
-        st.error(f"エラーが発生しました: {e}")
+df_test = pd.read_excel("test.xlsx")
+xlsx_test = df_to_xlsx(df_test)
 
-else:
-    st.info("Excelファイルがアップロードされていません。")
+st.download_button(label="Download", data=xlsx_test, file_name="_test.xlsx")
