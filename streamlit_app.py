f75f5c31580b12f44792ded706eedf4a41c4da17
NNN-TSUKIYAMA <162134115+NNN-TSUKIYAMA@users.noreply.github.com>
2024-03-04 22:21:30 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,5 @@
 import streamlit as st
-uploaded_file = st.file_uploader("ファイルをアップロードしてください。")
 
-import pandas as pd
-if uploaded_file is not None:
-    df = pd.read_csv(
-        uploaded_file,
-        sep=r'\s(?=(?:[^"]*"[^"]*")*[^"]*$)(?![^\[]*\])',
-        engine='python',
-        na_values='-',
-        header=None)
-    st.markdown('### アクセスログ（先頭5件）')
-    st.write(df.head(5))
+st.title("心拍変動解析ツール") # タイトル
+st.header("FFT") # ヘッダー
+
