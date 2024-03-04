01a20129e89735e7df90c3adaa1def0344706b7d
NNN-TSUKIYAMA <162134115+NNN-TSUKIYAMA@users.noreply.github.com>
2024-03-04 23:35:03 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,4 @@ def main():
     st.title("Excelデータ読み込みアプリ")
 
     # ファイルアップロード
-    uploaded_file = st.file_uploader("Excelファイルをアップロードしてください", type=['xlsx'])
+   uploaded_file = st.file_uploader("Excelファイルをアップロードしてください", type=['xlsx'])
